26ebb90b32b5da63514167d323fea900ea0ba432
richpk21 <richpk21@users.noreply.github.com>
2023-09-12 16:41:49 -0700

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("hello world in go!")
+	fmt.Println("hello world in go! Port :8080")
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is about page")
+		log.Println("This is about page")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!\n%s", r.URL.Path[1:], html.EscapeString(r.URL.Path))
+	log.Println(r.URL.Path)
 }
